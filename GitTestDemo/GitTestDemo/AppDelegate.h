995f8b5cbcba999494bfd0093825992c3298391c
shaojingdan <jingdan@staff.sina.com.cn>
2017-04-23 18:12:16 +0800

    Project init

---
+++ GitTestDemo/GitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTestDemo
+//
+//  Created by sina on 2017/4/23.
+//  Copyright © 2017年 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTestDemo/GitTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTestDemo
+//
+//  Created by sina on 2017/4/23.
+//  Copyright © 2017年 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
